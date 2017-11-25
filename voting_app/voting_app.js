8fecff066a550e2d2045209c9e30e09166aba23e
Rok <Rok>
2017-11-25 01:59:44 +0100

    fixed routing

--- voting_app/voting_app.js
+++ voting_app/voting_app.js
@@ -7,17 +7,24 @@ var Authentication = require("./controllers/authentication"),
     passportService = require("./services/passport"),
     passport = require("passport"),
     express = require("express"),
-    app = express.Router(),
+    app = express(),
     pug = require("pug"),
     // AUTHORIZE
     requireAuth = passport.authenticate("jwt", {session: false}),
     requireSignin = passport.authenticate("local", {session: false});
+
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "pug");
+    app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 
 /*  app.get("/", requireAuth, function(req, res) {
         res.send({hi: "there"});
     });*/
+    
+    
     app.get("/", function(req, res) {
         res.render("votingApp", { author: "ROK" })
     });
