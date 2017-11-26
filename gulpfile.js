cca14b38ee70093d81ab3143be8e52d1a82ba41f
Rok <Rok>
2017-11-26 01:25:59 +0100

    added minifier

--- gulpfile.js
+++ gulpfile.js
@@ -3,50 +3,47 @@ const gulp = require("gulp"),
       postcss = require("gulp-postcss"),
       autoprefixer = require("autoprefixer"),
       sass = require("gulp-sass"),
-      mainStylesPath = "./public/assets/styles/",
-      votingAppStylePath = "./voting_app/public/assets/styles";
+      mainStylePath = "./public/assets/styles/",
+      votingAppStylePath = "./voting_app/public/assets/styles/",
+      cleanCSS = require("gulp-clean-css");
+
       
 
-gulp.task("default", function() {
-  
-  console.log("Hooray - you created a gulp task!");  
-});
-
-
-// HTML
-gulp.task("html", function() {
- 
-});
-
-
-// SCSS
+// TRANSFORM SCSS
 gulp.task("homePageStyles", function() {
     
-    return gulp.src(mainStylesPath + "/scss/main.scss")
+    return gulp.src(mainStylePath + "scss/main.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer])) // order matters - FIFO (first in, first out)
-        .pipe(gulp.dest(mainStylesPath));
+        .pipe(gulp.dest(mainStylePath));
 });
-
 gulp.task("votingPageStyles", function() {
     
-    return gulp.src(votingAppStylePath + "/scss/main.scss")
+    return gulp.src(votingAppStylePath + "scss/main.scss")
         .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([autoprefixer])) // order matters - FIFO (first in, first out)
+        .pipe(postcss([autoprefixer ])) // order matters - FIFO (first in, first out)
         .pipe(gulp.dest(votingAppStylePath));
 });
-
+// MINIFY
+gulp.task("minifyLandingCSS", function() {
+    
+    return gulp.src(mainStylePath + "main.css")
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("./public/dist" ));
+});
+gulp.task("minifyVotingAppCSS", function() {
+    
+    return gulp.src(votingAppStylePath + "main.css")
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("./voting_app/public/dist"));
+});
 
 // WATCH
 gulp.task("watch", function() {
 
-    watch("./views/index.pug", function() {
-        gulp.start("html");
-    });
-    watch(mainStylesPath + "**/*.scss", function() {
-        gulp.start("homePageStyles");
-    });
-    watch(votingAppStylePath + "**/*.scss", function() {
-        gulp.start("votingPageStyles");
-    });
+    watch(mainStylePath + "**/*.scss", () => gulp.start("homePageStyles"));
+    watch(votingAppStylePath + "**/*.scss", () => gulp.start("votingPageStyles"));
+    watch(mainStylePath + "main.css", () => gulp.start("minifyLandingCSS"));
+    watch(votingAppStylePath + "main.css", () => gulp.start("minifyVotingAppCSS"));
+    
 });
