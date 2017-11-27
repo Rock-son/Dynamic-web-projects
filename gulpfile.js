cf34ac092bd11d1e81c6fd95cdac6bc6d72c7c25
Rok <Rok>
2017-11-27 01:50:01 +0100

    piped together some tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const gulp = require("gulp"),
       votingAppStylePath = "./voting_app/public/assets/styles/",
       cleanCSS = require("gulp-clean-css");
 
-      
+gulp.task('start', ['watch']);
 
 // TRANSFORM SCSS
 gulp.task("homePageStyles", function() {
@@ -15,6 +15,7 @@ gulp.task("homePageStyles", function() {
     return gulp.src(mainStylePath + "scss/main.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer])) // order matters - FIFO (first in, first out)
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest(mainStylePath));
 });
 gulp.task("votingPageStyles", function() {
@@ -22,28 +23,13 @@ gulp.task("votingPageStyles", function() {
     return gulp.src(votingAppStylePath + "scss/main.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer ])) // order matters - FIFO (first in, first out)
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest(votingAppStylePath));
-});
-// MINIFY
-gulp.task("minifyLandingCSS", function() {
-    
-    return gulp.src(mainStylePath + "main.css")
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("./public/dist" ));
-});
-gulp.task("minifyVotingAppCSS", function() {
-    
-    return gulp.src(votingAppStylePath + "main.css")
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("./voting_app/public/dist"));
 });
 
 // WATCH
 gulp.task("watch", function() {
 
     watch(mainStylePath + "**/*.scss", () => gulp.start("homePageStyles"));
-    watch(votingAppStylePath + "**/*.scss", () => gulp.start("votingPageStyles"));
-    watch(mainStylePath + "main.css", () => gulp.start("minifyLandingCSS"));
-    watch(votingAppStylePath + "main.css", () => gulp.start("minifyVotingAppCSS"));
-    
+    watch(votingAppStylePath + "**/*.scss", () => gulp.start("votingPageStyles"));    
 });
