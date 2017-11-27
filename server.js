a413ed58d4e5df31cb29fb2ce742c71229a17c8f
Rok <Rok>
2017-11-27 01:55:43 +0100

    new auth files routes

--- server.js
+++ server.js
@@ -7,19 +7,21 @@ const express = require("express"),
       path = require("path"),
       fs = require("fs"),
       bodyParser = require("body-parser"),
-      // templating and scss      
+      // AUTHENTICATION
+      Authentication = require("./auth/controllers/authentication"),
+      passportService = require("./auth/services/passport"),
+      passport = require("passport"),
+      requireAuth = passport.authenticate("jwt", {session: false}),
+      requireSignin = passport.authenticate("local", {session: false}),
+      // pug    
       pug = require("pug"),
       // ROUTES
       votingApp = require("./voting_app/voting_app"),
       // SECURITY
       fontArr = require("./public/assets/fonts/fontAllow"),
-      helmet = require("helmet"),
-      helmet_csp = require("helmet-csp"),  
-      /* LOGGING
-      morgan = require('morgan'),
-      Log = require("./logs/services/morganLog"), 
-      accessLogStream = fs.createWriteStream(path.join(__dirname, "logs", 'access.log'), {flags: 'a'}), // writable stream - for MORGAN logging   
-      */// DB
+      helmet = require("./security/helmet"),
+      // LOGGING:  morgan = require('morgan'),  Log = require("./logs/services/morganLog"), accessLogStream = fs.createWriteStream(path.join(__dirname, "logs", 'access.log'), {flags: 'a'}), // writable stream - for MORGAN logging
+      // DB
       mongoose = require("mongoose"),      
       dbUrl = process.env.DBLINK,
       // PORT & ROUTER
@@ -29,76 +31,77 @@ const express = require("express"),
 
 
 
-// App Setup
-app.use(bodyParser.json({type: "*/*"}));
-app.use(bodyParser.json({
-      type: ['json', 'application/csp-report']
-    }))
-//app.use(morgan({stream: accessLogStream}));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, 'public')));
+      // App Setup
+      app.use(bodyParser.json({type: "*/*"}));
+      app.use(bodyParser.json({
+            type: ['json', 'application/csp-report']
+      }))
+      //app.use(morgan({stream: accessLogStream}));
+      app.set("views", path.join(__dirname, "views"));
+      app.set("view engine", "pug");
+      app.use(express.static(path.join(__dirname, 'public')));
+
+      // SECURITY
+      helmet(app);
+
+
+      // ROUTING
+      app.use("/voting-app", votingApp);
+
+
+      // DB Setup
+      mongoose.connect(dbUrl);
 
 
 
-// SECURITY middleware (Helmet, Helmet-csp)
-app.use(helmet({dnsPrefetchControl: {allow: true}}));
-app.use(helmet_csp({
-directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"],
-      styleSrc: ["'self'", "https://cdnjs.cloudflare.com"],
-      fontSrc: ["'self'", "https://cdnjs.cloudflare.com"],
-      imgSrc: ["'self'", "https:", "www.dl.dropboxusercontent.com", "https://www.dl.dropboxusercontent.com", 'data:'],
-      sandbox: ['allow-forms', 'allow-scripts'],
-      reportUri: '/report-violation' // set up a POST route for notifying / logging data to server
-},
-reportOnly: function (req, res) {
-            if (req.query.cspmode === 'debug') {
-                  return true
-            } else {
-                  return false
-            }
-      }
-}));
-app.use(function(req, res, next) {
-      res.set({
-      "Access-Control-Allow-Origin" : "*",
-      "Access-Control-Allow-Headers" : "Origin, X-Requested-With, content-type, Accept"
+
+      app.get("/", function(req, res) {
+            res.render("index", {author: "ROK"})
       });
-      app.disable('x-powered-by');
-      next();
-});
 
 
-// ROUTING
-app.use("/voting-app", votingApp);
+      app.get("/public/assets/fonts/*", function(req, res) {
+            if (fontArr.indexOf(req.url.replace(/^public\/assets\/fonts\//, "") > -1)) {
+                  fs.createReadStream(path.join(__dirname, req.url)).pipe(res);
+            } else {
+                  res.status(204).send();
+            }
+      });
+
+      // GITHUB oAuth
+      app.get("/auth/github", function(req, res) {
+
+            //TODO: PUT IN SERVICES
+                  const CLIENT_ID = process.env.GITHUB_ID,
+                        GITHUB_SECRET = process.env.GITHUB_SECRET,
+                        SCOPE = "user:email",
+                        CSRF_KEY = process.env.GITHUB_CSRF_KEY,
+                        REDIRECT_URI = "https://fcc-dynamic-webapps-roky.herokuapp.com/voting_app";
+          
+               
+            console.log("not implemented yet!");
+            res.end();
 
 
-// DB Setup
-mongoose.connect(dbUrl);
+      });
+
+      /*  app.get("/", requireAuth, function(req, res) {
+        res.send({hi: "there"});
+    });*/
+    
+     
+    //TODO:!!!
+    // sign up
+    app.post("/signup", Authentication.signup);
+    // sign in
+    app.post("/signin", requireSignin, Authentication.signin);       
 
 
+            
+      // logging (Helmet-csp) CSP blocked requests
+      //app.post("/report-violation", Log.logged);
 
 
-app.get("/", function(req, res) {
-      res.render("index", {author: "ROK"})
-});
-
-
-app.get("/public/assets/fonts/*", function(req, res) {
-      if (fontArr.indexOf(req.url.replace(/^public\/assets\/fonts\//, "") > -1)) {
-            fs.createReadStream(path.join(__dirname, req.url)).pipe(res);
-      } else {
-            res.status(204).send();
-      }
-});
-
-
-// logging (Helmet-csp) CSP blocked requests
-//app.post("/report-violation", Log.logged);
-
-
-//Server Setup
-const server = http.createServer(app);
-server.listen(port, () => console.log("Listening on port: " + port));
+      //Server Setup
+      const server = http.createServer(app);
+      server.listen(port, () => console.log("Listening on port: " + port));
