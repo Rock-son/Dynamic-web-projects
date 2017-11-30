cc37b51b38f1457c1c252af90a3c5882ef6ca44e
Rok <Rok>
2017-11-30 02:38:01 +0100

    minor change

--- voting_app/voting_app.js
+++ voting_app/voting_app.js
@@ -4,9 +4,10 @@ var fs = require("fs"),
     path = require("path"),
     // PASSPORT service
     express = require("express"),
-    app = express(),
     pug = require("pug"),
-    indexCSS = "./assets/styles/index.css";
+    indexCSS = "./assets/styles/index.css",
+    
+    app = express();
 
     app.set("views", path.join(__dirname, "views"));
     app.set("view engine", "pug");
