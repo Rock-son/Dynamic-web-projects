2476e2d34e229d03a083a55e1aea516745d4e85b
rok <roky.zz@gmail.com>
2017-12-06 21:45:22 +0100

    added babel core, loader, es2015 and rest spread operator

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "./public/dist"),
         filename: "main.js"
+    },
+    module: {
+        rules: [
+            {            
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/	        
+            }
+        ]
     }
 }
