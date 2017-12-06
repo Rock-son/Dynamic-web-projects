9235ca25bafd8e60a564eed33a421b502b769a53
rok <roky.zz@gmail.com>
2017-12-07 00:04:35 +0100

    reveal on scroll script

---
+++ public/assets/scripts/RevealOnScroll.js
@@ -0,0 +1,31 @@
+import waypoints from "../../node_modules/waypoints/lib/noframework.waypoints";
+import classList from "classlist-polyfill";
+classList();
+
+
+class RevealOnScroll {
+    constructor(items, offset) {
+        this.itemsToReveal = items;
+        this.offsetPercentage = offset;
+        this.hideInitially();
+        this.createWaypoints();
+    }
+
+    hideInitially() {
+        this.itemsToReveal.classList.add("reveal-item");
+    }
+
+    createWaypoints() {
+        const self = this;
+        this.itemsToReveal.forEach(function(element) {
+            const currentItem = this;
+            new Waypoint({
+                element: currentItem,
+                handler: function() {
+                    currentItem.classList.add("reveal-item--is-visible");
+                },
+                offset: self.offsetPercentage
+            });
+        });
+    }
+}
