8d53923fa384914103e8764500ea2342f87b1b34
rok <roky.zz@gmail.com>
2017-12-07 01:15:24 +0100

    added page section waypoints + smooth scrolling (lib)

--- public/assets/scripts/StickyHeaders.js
+++ public/assets/scripts/StickyHeaders.js
@@ -1,15 +1,29 @@
+"use strict"
 import waypoints from "../../node_modules/waypoints/lib/noframework.waypoints";
+import smoothScroll from "jquery-smooth-scroll";
 import classList from "classlist-polyfill";
 classList();
 
+
+
 class StickyHeader {
+
     constructor(element, parent) {
+        this.pageSections = document.getElementsByTagName("section");
+        this.headerLinks = document.getElementsByClassName("nav-headers");
         this.siteHeader = element;
         this.headerTriggerElement = parent;
         this.createHeaderWaypoint();
+        this.createPageSectionWaypoints();
+        this.addSmoothScrolling();
+    }
+
+    addSmoothScrolling() {
+        this.headerLinks.smoothScroll();
     }
 
     createHeaderWaypoint() {
+
         const that = this;
         new Waypoint({
             element: this.headerTriggerElement,
@@ -20,6 +34,37 @@ class StickyHeader {
                     that.siteHeader.classList.remove("site-header--dark");
                 }
             }
+        });
+    }
+
+    createPageSectionWaypoints() {
+        
+        const that = this;
+        this.itemsToReveal.forEach(function(element) {
+            const currentPageSection = this;
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if (direction === "down") {
+                        const matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.classList.remove("is-current-link");
+                        document.getElementById(matchingHeaderLink).classList.add("is-current-link");
+                    }
+                },
+                offset: "18%"
+            });
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if (direction === "up") {
+                        const matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.classList.remove("is-current-link");
+                        document.getElementById(matchingHeaderLink).classList.add("is-current-link");
+                    }
+                },
+                offset: "-40%"
+            });
+
         });
     }
 }
