93cd4f5c49d26a17d066b27dab877d93f487287e
rok <roky.zz@gmail.com>
2017-12-09 04:54:44 +0100

    rename scss to partial - fixed dependencies

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,12 +1,12 @@
 const gulp = require("gulp"),
       watch = require("gulp-watch"),
       // OUTPUT DIRs
-      homeOutputPath = "./public/assets/styles/",
-      votingOutputPath = "./voting_app/public/assets/styles/",
+      homeOutputPath = "./public/assets/styles/partial/",
+      votingOutputPath = "./voting_app/public/assets/styles/partial/",
       // STARTING DIRs
-      homeStylePath = homeOutputPath + "scss/home/",
-      registerStylePath = homeOutputPath + "scss/register/",
-      votingAppStylePath = votingOutputPath + "scss/",
+      homeStylePath = homeOutputPath + "home/",
+      registerStylePath = homeOutputPath + "register/",
+      votingAppStylePath = votingOutputPath + "index/",
       scriptsPath = "./public/assets/scripts/",      
       votingScriptsPath = "./voting_app/public/assets/scripts/"
 
@@ -16,9 +16,9 @@ gulp.task('start', ['watch']);
 gulp.task("watch", function() {
     
     watch(homeStylePath + "*.scss", () => gulp.start("homePageStyles"));
-    watch(homeOutputPath + "**/*.scss", () => gulp.start("homePageStyles", "registerPageStyles"));
-    watch("./public/assets/styles/shared/*.scss", () => gulp.start(["homePageStyles", "votingPageStyles", "registerPageStyles"]));
-    watch(votingAppStylePath + "**/*.scss", () => gulp.start("votingPageStyles"));
+    watch(registerStylePath + "*.scss", () => gulp.start("registerPageStyles"));
+    watch(homeOutputPath + "shared/*.scss", () => gulp.start(["homePageStyles", "votingPageStyles", "registerPageStyles"]));
+    watch(votingOutputPath + "**/*.scss", () => gulp.start("votingPageStyles"));
     watch(scriptsPath + "*.js", () => gulp.start("scripts"));
     watch(votingScriptsPath + "*.js", () => gulp.start("scriptsVoting"));
 });
