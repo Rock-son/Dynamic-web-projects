3dbb01ece2af75641fbc7c77129fab18443b9833
roky <roky.zz@gmail.com>
2018-01-11 02:33:23 +0100

    added regex for css and js files

--- voting_app/voting_app.js
+++ voting_app/voting_app.js
@@ -2,12 +2,16 @@
 
 var fs = require("fs"),
     path = require("path"),
-    //DB 
-    db = require("../db/controllers/controller"),
-    // PASSPORT service
     express = require("express"),
     pug = require("pug"),
-    homeCSS = "./dist/homePage.css", createPollCSS = "./dist/createPoll.css",
+    // DB
+    db = require("../db/controllers/controller"),
+    // ASSETS
+    homeCSS = "./dist/homePage.css", 
+    createPollCSS = "./dist/createPoll.css",
+    mainJS = (function() {return "./dist/"+ (/(main_[-\w]*\.js)/.exec(fs.readdirSync(path.resolve(__dirname, "public/dist/")).join(",")) || ["main.js"] )[0] })(),
+    createPollJS = (function() {return "./dist/"+ (/(createPoll_[-\w]*\.js)/.exec(fs.readdirSync(path.resolve(__dirname, "public/dist/")).join(",")) || ["createPoll.js"] )[0] })(),
+    // PASSPORT
     passportService = require("../auth/services/passport"),
     passport = require("passport"),
     escapeHtml = require('escape-html'),
@@ -15,7 +19,6 @@ var fs = require("fs"),
     app = express(),
 
     ensureAuthenticated = passport.authenticate('jwt', {session: false, failureRedirect: "/auth/login"});
-    
     
     app.set("views", path.join(__dirname, "views"));
     app.set("view engine", "pug");
@@ -38,13 +41,14 @@ var fs = require("fs"),
             passport.authenticate('jwt', {session: false, failureRedirect: "/auth/login"}, function(err, user, info, status) {
                 if (err) { return next(err) }
                 if (!user) { return res.redirect("/auth/login"); }
-                return res.render("createPoll", { cssPath: createPollCSS, auth: true, user: escapeHtml(user.username || user.displayName) });
+                return res.render("createPoll", { js: createPollJS, cssPath: createPollCSS, auth: true, user: escapeHtml(user.username || user.displayName) });
             })(req, res, next);
         })
         .post(ensureAuthenticated, db.insertFormData);
 
 
-    app.get("/myPoll:poll", function(req, res, next) {
+    app.get("/poll:id", function(req, res, next) {
+        
 
     });
 
