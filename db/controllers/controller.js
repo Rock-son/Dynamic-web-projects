9847590e69f2ebcd21f4fa82a91a15b4cead4c66
roky <roky.zz@gmail.com>
2018-01-14 14:09:10 +0100

    added manage poll and get poll methods

--- db/controllers/controller.js
+++ db/controllers/controller.js
@@ -1,27 +1,32 @@
 const { PollSchema } = require("../models/poll"),
         mongoSanitize = require("mongo-sanitize"),
-        createHash = require("./_createHash");
+        createHash = require("./_createHash"),
+        xssFilters = require("xss-filters"),
+        // PASSPORT
+        passport = require("passport");
 
 
 
 exports.insertFormData = function(req, res, next) {   
     
-    const createdBy = mongoSanitize(req.body.createdBy || "").trim();
-          optsArr = Array.isArray(req.body.options) ? req.body.options : [req.body.options],
-          options = optsArr.map((item) => mongoSanitize(item));
+    const createdBy = xssFilters.inHTMLData(mongoSanitize( req.body.createdBy || "" ).trim()),
+          title = xssFilters.inHTMLData(mongoSanitize( req.body.poll_title || "" ).trim()),
+          optsArr = Array.isArray( req.body.options ) ? req.body.options : [req.body.options],          
+          options = optsArr.map( ( item ) => xssFilters.inHTMLData(mongoSanitize( item )) );
           
     // CHECK IF EMPTY
     if (createdBy == "") {
         return res.redirect('/auth/login');  
-    } else if (options.length === 0) {
-        return res.status(422).send({error: "Sent form must include at least one voting option!"});
+    } else if ( options.length === 0 || title === "" ) {
+        return res.status( 422 ).send( { error: "Sent form must include at least one voting option and title!" } );
     }
 
     const data = Date.now().toString() + Math.random().toString(),
           opts = options.filter((item) => item !== "").map( (item) => [item, 0]),
           url  = createHash("md5", data),
-          poll = new PollSchema({ 
-                    createdBy, 
+          poll = new PollSchema({
+                    createdBy,
+                    title,
                     options: opts, 
                     url 
                 });
@@ -32,3 +37,26 @@ exports.insertFormData = function(req, res, next) {
         return res.status(302).set({"Location": "/poll?" + poll.url}).end();
     });
 };
+
+exports.managePollData = function(req, res, next, options) {
+    
+    const id = xssFilters.uriComponentInHTMLData(mongoSanitize(req.query.url));
+    
+    PollSchema.findOne({url: id}, function(err, poll) {
+        if (err) next(err);
+        
+        options.data = poll;
+        return res.render("poll", options);
+    });
+}
+
+exports.getPollData = function(req, res, next) {
+
+    const id = xssFilters.uriComponentInHTMLData(mongoSanitize(req.query.url));
+    
+    PollSchema.findOne({url: id}, function(err, poll) {
+        if (err) next(err);
+        
+        return res.set({"Content-Type": "application/json"}).send({"poll": poll});
+    });
+}
