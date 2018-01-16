d34c9107fac4a6c3ea1f20f22f034fb727f5ebb4
roky <roky.zz@gmail.com>
2018-01-16 15:39:16 +0100

    added method - in work

--- db/controllers/controller.js
+++ db/controllers/controller.js
@@ -7,7 +7,7 @@ const { PollSchema } = require("../models/poll"),
 
 
 
-exports.insertFormData = function(req, res, next) {   
+exports.insertPollData = function(req, res, next) {
     
     const createdBy = xssFilters.inHTMLData(mongoSanitize( req.body.createdBy || "" ).trim()),
           title = xssFilters.inHTMLData(mongoSanitize( req.body.poll_title || "" ).trim()),
@@ -37,6 +37,38 @@ exports.insertFormData = function(req, res, next) {
         return res.status(302).set({"Location": "./poll?url=" + poll.url}).end();
     });
 };
+
+exports.updatePollOPtions = function(req, res, next) {
+    
+    const createdBy = xssFilters.inHTMLData(mongoSanitize( req.body.createdBy || "" ).trim()),
+          title = xssFilters.inHTMLData(mongoSanitize( req.body.poll_title || "" ).trim()),
+          optsArr = Array.isArray( req.body.options ) ? req.body.options : [req.body.options],          
+          options = optsArr.map( ( item ) => xssFilters.inHTMLData(mongoSanitize( item )) );
+          
+    // CHECK IF EMPTY
+    if (createdBy == "") {
+        return res.redirect('/auth/login');  
+    } else if ( options.length === 0 || title === "" ) {
+        return res.status( 422 ).send( { error: "Sent form must include at least one voting option and title!" } );
+    }
+
+    const data = Date.now().toString() + Math.random().toString(),
+          opts = options.filter((item) => item !== "").map( (item) => [item, 0]),
+          url  = createHash("md5", data),
+          poll = new PollSchema({
+                    createdBy,
+                    title,
+                    options: opts, 
+                    url 
+                });
+                
+    poll.save(function(err) {
+        if (err) return next(err)
+        
+        return res.status(302).set({"Location": "./poll?url=" + poll.url}).end();
+    });
+};
+
 
 exports.managePollData = function(req, res, next, options) {
     
