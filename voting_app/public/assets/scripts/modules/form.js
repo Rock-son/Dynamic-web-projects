9885ba4b79614dce5aee43da2a37d589893a6bc3
rok <roky.zz@gmail.com>
2018-01-19 15:20:48 +0100

    finished form logic

--- voting_app/public/assets/scripts/modules/form.js
+++ voting_app/public/assets/scripts/modules/form.js
@@ -2,9 +2,9 @@ import { basename } from "path";
 
 module.exports = function(e) {
     // ELEMENT IDs
-    const CANCEL_OPTION = "cancel_option",
+    const CANCEL_BTN = "cancel_option",
           SELECTED_OPTION = "new_option",
-          ADD_OPTION = "add",
+          ADD_BTN = "add",
           DELETE = "delete",
           SUBMIT = "submit",
           FIELDSET = "fs",
@@ -21,25 +21,28 @@ module.exports = function(e) {
         // MSIE hack
         if (window.event) e = window.event;
 
-        const addBtn = document.getElementById(ADD_OPTION),
-              deleteBtn = document.getElementById(DELETE),
-              submitBtn = document.getElementById(SUBMIT),
-              fieldset = document.getElementById(FIELDSET),
+        const fieldset = document.getElementById(FIELDSET),
               id = parseInt(e.target.id != null ? e.target.id : 0),
               idIsNaN = !(id === id); // isNaN is the only thing in JS that is not equal to itself!
         
         if ( e.target.id != null && idIsNaN ) {
             switch (e.target.id) {
-                case ADD_OPTION:
-                    addNewOption(fieldset, addBtn);
+                case ADD_BTN:
+                    addNewOption(fieldset);
                     break;
-                case CANCEL_OPTION:
+                case CANCEL_BTN:
                     cancelNewOption(e);
                     break;
                 case SELECTED_OPTION:
                     closeWarning(e);
                 case SUBMIT:
-                    submit(e);
+                    submitForm(e);
+                    break;
+                case DELETE + "_btn":
+                    showWrapper(fieldset);
+                    break;
+                case DELETE:
+                    fieldset.lastChild.click();
                     break;
                 default:
                     break;
@@ -56,13 +59,21 @@ module.exports = function(e) {
         }        
     }
 
-    function addNewOption(fieldset, addBtn) {
+    function addNewOption(fieldset) {
 
-        if (document.getElementById(SELECTED_OPTION)) return;
+        // LEAVE NEW OPT INPUT (if open)
+        if (document.getElementById(SELECTED_OPTION)) {
+            return;
+        }
+        // CLOSE DELETE INPUT (if open)
+        if (document.getElementById(DELETE)) {
+            document.getElementById(CANCEL_BTN).click();
+            return;
+        }
         if (document.querySelector(".form__options.selected")) {
             document.querySelector(".form__options.selected").className = "form__options";
         } 
-
+        closeWarning(e);
 
         const input = document.createElement("input"),
               cancelBtn = document.createElement("div"),
@@ -74,7 +85,7 @@ module.exports = function(e) {
         input.name = "newOption";
 
         cancelBtn.className = "form__wrapper__cancel btn btn--medium btn--full";
-        cancelBtn.setAttribute("id", CANCEL_OPTION);
+        cancelBtn.setAttribute("id", CANCEL_BTN);
         cancelBtn.innerHTML = "Cancel";
 
         wrapper.appendChild(input);
@@ -99,7 +110,11 @@ module.exports = function(e) {
         e.target.style.color = "#fff";
         e.target.style.textShadow = "none";
 
-        setTimeout(function() { wrapper.removeChild(wrapper.lastChild); }, 50);
+        setTimeout(function() { 
+            wrapper.removeChild(wrapper.lastChild);
+            wrapper.firstChild.value = "";
+            wrapper.firstChild.style.width = "0px";
+        }, 50);
         setTimeout(function() { 
             fieldset.removeChild(fieldset.lastChild.previousSibling); 
             fieldset.lastChild.value = "Submit Vote";
@@ -118,10 +133,11 @@ module.exports = function(e) {
 
         // WHEN SELECTING - deselect each item and close new option if exists
         const list = document.getElementsByClassName("form__options"),
-              newOption = document.getElementById(SELECTED_OPTION) || null,
+              newOption = document.getElementById(SELECTED_OPTION),
+              deleteOption = document.getElementById(DELETE),
               len = list.length;
         for (let i = 0; i < len; i++) { list.item(i).className = FORM_OPTIONS; }
-        if (newOption != null) {document.getElementById("cancel_option").click();}
+        if (newOption || deleteOption) {document.getElementById(CANCEL_BTN).click();}
 
         // ADD "SELECTED" CLASS TO "MESSENGER" DIV
         e.target.className = e.target.className + " selected";        
@@ -136,7 +152,7 @@ module.exports = function(e) {
         }
     }
 
-    function submit(e) {
+    function submitForm(e) {
 
         const EMPTY_NEW_OPTION = "empty", NO_VALUE = "no_value";
 
@@ -149,38 +165,68 @@ module.exports = function(e) {
                 e.preventDefault();
                 showError(EMPTY_NEW_OPTION);
             }
-        // ELSE CHECK ACTIVE VALUE
-        } else {
-            if (document.querySelector(".form__options.selected") == null) {
+        // ELSE CHECK ACTIVE VALUE OR EXISTANCE OF DELETE BTN
+        } else if (!document.querySelector(".form__options.selected") && !document.getElementById("delete")) {
                 e.preventDefault();
                 showError(NO_VALUE);
-            }
         }
 
+                function showError(type) {
 
+                    const fieldset = document.getElementById("fs"),
+                            error = document.createElement("div");
 
-        function showError(type) {
+                    error.setAttribute("id", "warning");
+                    error.style.color = "red";
+                    switch (type) {
+                        case EMPTY_NEW_OPTION:
+                            error.innerHTML = "Input new option or close the input field!";
+                            break;
+                        case NO_VALUE:
+                            error.innerHTML = "Please select or add one option!";
+                            break;
+                        default:
+                            error.innerHTML = "Unexpected error. Try reloading the page.";
+                            break;
+                    }            
 
-            const fieldset = document.getElementById("fs"),
-                  error = document.createElement("div");
+                    fieldset.insertBefore(error, fieldset.lastChild);
+                }            
+    }
 
-            error.setAttribute("id", "warning");
-            error.style.color = "red";
-            switch (type) {
-                case EMPTY_NEW_OPTION:
-                    error.innerHTML = "Input new option or close the input field!";
-                    break;
-                case NO_VALUE:
-                    error.innerHTML = "Please select or add one option!";
-                    break;
-                default:
-                    error.innerHTML = "Unexpected error. Try reloading the page.";
-                    break;
-            }            
+    function showWrapper(fieldset) {
 
-            fieldset.insertBefore(error, fieldset.lastChild);
+        // CLOSE NEW INPUT (if open)
+        if (document.getElementById(SELECTED_OPTION)) {
+            document.getElementById(CANCEL_BTN).click();
+            return;
         }
+        // LEAVE OPEN DELETE INPUT
+        if (document.getElementById(DELETE)) return;
 
+        // DESELECT ANY SELECTED OPTIONS
+        if (document.querySelector(".form__options.selected")) {
+            document.querySelector(".form__options.selected").className = "form__options";
+        } 
+
+
+        const input = document.createElement("input"),
+              cancelBtn = document.createElement("div"),
+              wrapper = document.createElement("div");
+
+        input.className = "form__wrapper__delete btn btn--medium btn--full";
+        input.value = "Go ahead!"
+        input.name = "delete_poll"
+        input.setAttribute("id", DELETE);
+
+        cancelBtn.className = "form__wrapper__cancel-delete btn btn--medium btn--full";
+        cancelBtn.setAttribute("id", CANCEL_BTN);
+        cancelBtn.innerHTML = "Cancel";
+
+        wrapper.appendChild(input);
+        wrapper.appendChild(cancelBtn);
+        wrapper.className = "form__wrapper";
+
+        fieldset.insertBefore(wrapper, fieldset.lastChild);
     }
 }
-
