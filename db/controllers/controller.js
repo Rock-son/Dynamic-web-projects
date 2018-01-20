d4f4f80bc7967c39fcf1a7491e02e81498ea4f46
rok <roky.zz@gmail.com>
2018-01-20 02:45:33 +0100

    added delete poll method

--- db/controllers/controller.js
+++ db/controllers/controller.js
@@ -1,3 +1,5 @@
+import { basename } from "path";
+
 const { PollSchema } = require("../models/poll"),
         mongoSanitize = require("mongo-sanitize"),
         createHash = require("./modules/_createHash"),
@@ -8,12 +10,16 @@ const { PollSchema } = require("../models/poll"),
 
 // CREATE NEW POLL
 exports.insertPollData = function(req, res, next) {
-    
+
     const createdBy = xssFilters.inHTMLData(mongoSanitize( req.body.createdBy || "" ).trim()),
           title = xssFilters.inHTMLData(mongoSanitize( req.body.poll_title || "" ).trim()),
           optsArr = Array.isArray( req.body.options ) ? req.body.options : [req.body.options],          
           options = optsArr.map( ( item ) => xssFilters.inHTMLData(mongoSanitize( item )) );
-          
+
+    // CHECK FOR ARRAYS, WHERE THERE SHOULD BE NONE!
+    createdBy = Array.isArray(createdBy) ? createdBy.slice(-1)[0] : createdBy;
+    title = Array.isArray(title) ? title.slice(-1)[0] : title;
+
     // CHECK IF EMPTY
     if (createdBy == "") {
         return res.redirect('/auth/login');  
@@ -39,12 +45,34 @@ exports.insertPollData = function(req, res, next) {
 };
 // FOR VOTING SITE
 exports.updatePollOptions = function(req, res, next) {
+
+    const voted_option = xssFilters.inHTMLData(mongoSanitize( req.body.voted || "" ).trim()),
+          shouldPollBeDeleted = xssFilters.inHTMLData(mongoSanitize( req.body.delete_poll || !!0 )),          
+          id = xssFilters.uriComponentInHTMLData(mongoSanitize(req.body.poll)),
+          username = req.user.username;
+
+    // DELETE POLL IF OPTED
+    if (shouldPollBeDeleted) {
+
+        PollSchema.findById({_id: id}, function(err, poll) {
+            if (err) next(err);
+
+            if ( username === poll.createdBy ) {
+                PollSchema.remove({_id: id}, function(err) {
+                    if (err) next(err);                    
+                    return res.status(302).set( { "Location": "./myPolls" } ).end();
+                });
+            } else {
+                return res.status( 422 ).send( { error: "You are not the owner of the poll and can therefore not delete it!" } );
+            }
+        });
+    }
+
+
+    // CHECK FOR ARRAYS, WHERE THERE SHOULD BE NONE!
+    voted_option = Array.isArray(voted_option) ? voted_option.slice(-1)[0] : voted_option;
+    id = Array.isArray(id) ? id.slice(-1)[0] : id;
     
-    return res.status( 422 ).send( { voted: req.body.voted, delete: req.body.delete_poll} );
-    const title = xssFilters.inHTMLData(mongoSanitize( req.body.poll_title || "" ).trim()),
-          optsArr = Array.isArray( req.body.options ) ? req.body.options : [req.body.options],          
-          options = optsArr.map( ( item ) => xssFilters.inHTMLData(mongoSanitize( item )) );
-          
     // CHECK IF EMPTY
     if (createdBy == "") {
         return res.redirect('/auth/login');  
@@ -67,10 +95,10 @@ exports.updatePollOptions = function(req, res, next) {
         
         return res.status(302).set({"Location": "./poll?url=" + poll.url}).end();
     });
-};
+}
 
 // RETURN DATA FOR SPECIFIC POLL (/poll?url=j73jhn3s...)
-exports.managePollData = function(req, res, next, options) {
+exports.showPollData = function(req, res, next, options) {
     
     const url = Array.isArray(req.query.url) ? req.query.url.slice(-1)[0] : req.query.url,
           id = xssFilters.uriComponentInHTMLData(mongoSanitize(url));
@@ -81,7 +109,7 @@ exports.managePollData = function(req, res, next, options) {
         if (!poll) {
             return res.render({"error": "No poll with url: " + id + " found!"});
         }
-        options.data =hideID(poll);
+        options.data =poll;
         return res.render("poll", options);
     });
 }
@@ -95,7 +123,7 @@ exports.showMyPolls = function(req, res, next, options) {
         if (!polls.length) {
             return res.render({"error": "No polls found!"});
         }
-        options.polls =hideID(polls);
+        options.polls =polls;
         return res.render("homePage", options);
     });
 }
@@ -103,11 +131,12 @@ exports.showMyPolls = function(req, res, next, options) {
 function hideID(json) {
         
     if (Array.isArray(json)) {
-
+        let result = [];
         json.forEach((poll,index) => {
             delete poll._doc._id;
-            return poll._doc;
+            result.push(poll._doc);
         });
+        return result;
     } else {
         delete json._doc._id;
         return json._doc;
