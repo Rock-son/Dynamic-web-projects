e1ae3e9d3f1336fbd6efa8f9879b5ec7ded80913
rok <roky.zz@gmail.com>
2018-01-21 01:48:25 +0100

    added get ip method (module)

---
+++ db/controllers/modules/getIp.js
@@ -0,0 +1,6 @@
+"use strict"
+
+module.exports = function(req) {
+    return req.headers['x-forwarded-for'].split(',').pop() || req.connection.remoteAddress || 
+    req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null);
+};
